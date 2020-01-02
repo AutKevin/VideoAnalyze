c5b3177681fa3863715169a5bbb0e86aa7e1d148
autumn <AutKevin@users.noreply.github.com>
2020-01-02 23:09:16 +0800

    向指定页面注入contentjs

--- script/yunjiexi.js
+++ script/yunjiexi.js
@@ -6,27 +6,22 @@ $(document).ready(function(){
 			var nowUrl="https:"+$("link[rel=canonical]").attr("href");
 			//抓取当前播放页播放视频的名称
 			var nowTitle=$("meta[name=irTitle]").attr("content");
-			var btnpt='.videoArea:eq(0)';
 		break;
 		case "v.youku.com":
 			var nowUrl=window.location.href;
 			var nowTitle=$("#subtitle").text();
-			var btnpt='.playArea';
 		break;
 		case "www.le.com":
 			var nowUrl=$("link[rel=canonical]").attr("href");
 			var nowTitle=$("meta[name=irTitle]").attr("content");
-			var btnpt='#fla_box';
 		break;
 		case "www.mgtv.com":
 			var nowUrl=$("link[rel=canonical]").attr("href");
 			var nowTitle=$("meta[name=irTitle]").attr("content");
-			var btnpt='#fla_box';
 		break;
 		default:
-			var nowUrl="http://www.baidu.com";
-			var nowTitle="未匹配到额~";
-			var btnpt='#yunjiexi';
+			var nowUrl=window.location.href;
+			var nowTitle=$("title").html();
 
 	}
 	
@@ -35,23 +30,11 @@ $(document).ready(function(){
 		//页面通信
 		chrome.runtime.sendMessage(nowData, function(response){
 			//{nowUrl: "https://www.iqiyi.com/v_19rvh2ahsw.html", nowTitle: "被光抓走的人"}
-			console.log(nowData);
 			//{jiexiUrL: "https://mzrdy.applinzi.com/mzr/vipparse/index.php?url=", btnstatus: "close"}
+			console.info("--------内容页向后台页发送数据nowData-------");
+			console.log(nowData);
+			console.info("--------内容页收到后台页response数据-------");
 			console.log(response);
-			if(response.btnstatus=="open"){
-				$vippaly = $('<div class="yunjiexi" style="z-index:999999;position: absolute;top:20px;left: 10px;padding: 10px 20px;font-size: 18px;background: rgba(255, 255, 255, 0.78);border-radius:6px;"><a href="'+response.jiexiUrL+nowUrl+'" style="padding-left: 24px;background:url(http://toupian.dulihu.com/mzryun3.png) no-repeat">云解析</a><span style="position: absolute;right: 0;top: 0;background: #fff;padding: 0 2px;cursor: Default;">&times;</span></div>'); 
-				$(btnpt).append($vippaly);
-			}
 		});
 	}
-	/*
-	//动态更改右键菜单
-	window.oncontextmenu = function(){
-	    chrome.runtime.sendMessage(nowData,function(response){
-			console.log(nowData);
-		});
-	}*/
-	$(document).on("click",".yunjiexi span",function(){
-		$(".yunjiexi").css("display","none");
-	})
 });
