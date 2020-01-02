1aa92371c9f5691f79a121a1e396911b96b51eba
autumn <AutKevin@users.noreply.github.com>
2020-01-02 23:09:59 +0800

    后台js

--- script/background.js
+++ script/background.js
@@ -9,20 +9,22 @@ chrome.contextMenus.create({
     'documentUrlPatterns':["*://www.iqiyi.com/*","*://v.youku.com/*","*://www.le.com/*","*://*.mgtv.com/*","*://*.v.qq.com/*","*://*.tudou.com/*","*://*.wasu.cn/*","*://*.ku6.com/*","*://*.56.com/*","*://*.tv.sohu.com/*","*://*.miaopai.com/*","*://*.1905.com/*","*://*.pptv.com/*","*://*.baofeng.com/*","*://*.bilibili.com/*","*://*.fun.tv/*","*://*.acfun.cn/*","*://*.kuaishou.cn/*"],
     'onclick':urlJiexi
 });
+
 //更改右键菜单
 //chrome.contextMenus.update('jiexi',{'title':'Aeolian影视云 ['+message.nowTitle+']'});
 //Aeolian影视云，邮件菜单的onclick事件，包含很多信息https://developer.chrome.com/extensions/contextMenus#event-onClicked
 function urlJiexi(info,tab){
 	var url = jiexiUrL+info.pageUrl;
-	//localStorage.setItem("videourl",url);
     window.open(url, '_blank');
 }
+
 //接收urldata
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	nowData=JSON.stringify(message);
+	console.info("--------后台页接收到内容页数据-------");
+	console.info(nowData);
+	
 	localStorage.setItem("nowData",nowData);
-    var reData={"jiexiUrL":jiexiUrL,"btnstatus":localStorage.btn};
+    var reData={"jiexiUrL":jiexiUrL};
     sendResponse(reData);
 });
-
-
