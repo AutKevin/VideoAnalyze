3f1007a8f9a9fa9094d17b9b1e95c23b66130e4e
autumn <AutKevin@users.noreply.github.com>
2020-01-02 23:08:20 +0800

    弹出页后台js
    弹出页后台js

--- script/popup.js
+++ script/popup.js
@@ -1,29 +1,27 @@
 //定义解析的url
 var jiexiUrL="https://mzrdy.applinzi.com/mzr/vipparse/index.php?url=";
+//二维码
+var qrUtil= 'http://qr.liantu.com/api.php?text=';
 
  $(document).ready(function(){
- 	nowData=localStorage.nowData?JSON.parse(localStorage.nowData):null;
-    //console.log(nowData);
+    /*
+    nowData=localStorage.nowData?JSON.parse(localStorage.nowData):null;
+    console.log("------------------popup.js------------------");
+    console.log(nowData);
+	
     if(nowData){
-	  console.info("nowData:"+nowData);
       $("#title").append(nowData.nowTitle);
-      $("#qrcode").attr('src','http://qr.liantu.com/api.php?text='+jiexiUrL+nowData.nowUrl);  //url转为二维码
+      $("#qrcode").attr('src',qrUtil+jiexiUrL+nowData.nowUrl);  //url转为二维码
     }else{
-      $("#title").append("你还未观看过视频");
-      $("#qrcode").attr('src','../images/icons128.png');
-    }
-  	//按钮设置
-  	var btnstatus=(localStorage.btn == undefined)?"open":localStorage.btn;
-  	if(btnstatus=="close"){
-  		$(".jiexibt input").attr("checked","checked");
-  	}
-  	$(".jiexibt input").on("click",function(){
-  		var btn=$('input:checked').val();
-  		if (btn){
-  			localStorage.btn="close";
-  		}else{
-  			localStorage.btn="open";
-  		}
-  	})
+      $("#title").append("当前页面二维码");
+      $("#qrcode").attr('src',qrUtil + window.location.href);
+    }*/
+	
+    /*直接获取当前页面url并转换为二维码*/
+    chrome.tabs.getSelected(null, function (tab) {
+	var tabUrl = tab.url;
+	$("#title").append("当前页面二维码");
+	$("#qrcode").attr('src',qrUtil + tabUrl);
+    });
 });
 
